07ff7688d0e8b0f1d4847734d03041eeeab5d5e4
Azizbek-h <texnoposnode@gmail.com>
2022-11-11 01:33:46 +0500

    commit

--- handler/group/new_left_chat_member.py
+++ handler/group/new_left_chat_member.py
@@ -2,8 +2,9 @@ from aiogram import types
 from config.config import dp
 from filters.bot_admin_chat import IsAdmin
 from database.connect import reg_group, new_member
+from aiogram.dispatcher.filters import ChatTypeFilter
 
-@dp.message_handler(IsAdmin(), content_types=['new_chat_members', 'left_chat_member'])
+@dp.message_handler(IsAdmin(), content_types=['new_chat_members', 'left_chat_member'], chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
 async def logika(message: types.Message):
     reg_group(message.chat.id)
     try:
